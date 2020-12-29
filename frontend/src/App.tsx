99bb544070374939a2e337eb496bb43bdbfa0c62
Eduardo <edusanto22@gmail.com>
2020-12-29 08:22:23 -0300

    Componentes testes criados

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,23 @@
-import React from 'react';
-import Home from './components/Home';
+import React, {  Fragment  } from 'react';
+import Header from './components/Header';
+import Menu from './components/Menu';
 
 function App() {
   return (
-    <>
-      <Home />
+    //usando o Fragment pq é obrigatório
+    <Fragment>
+      <Header title="Página inicial"/>
       <h1>Hello wolrd!</h1>
-    </>
+
+      <Menu>
+        <ul>
+          <li>Menu 1</li>
+          <li>Menu 2</li>
+          <li>Menu 3</li>
+        </ul>
+      </Menu>
+      
+    </Fragment>
   );
 }
 
