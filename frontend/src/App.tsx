f467447663982447ec6922bd72d99f8fd6de60a0
Eduardo <edusanto22@gmail.com>
2020-12-31 01:33:35 -0300

    Inicio de projeto

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,16 @@
 import React, {  Fragment, useState  } from 'react';
-import Header from './components/Header';
-import Menu from './components/Menu';
+import "./App.css";
+
+import Home from "./pages/Home/index";
 
 function App() {
-
-  const [counter, setCounter] = useState(0);
-
-  const incrementar:any = () =>{
-    setCounter(counter + 1);
-
-    console.log(counter);
-  }
 
   return (
     //usando o Fragment pq é obrigatório
     <Fragment>
-      <Header title="Página inicial"/>
-      <h1>Hello wolrd!</h1>
 
-      <Menu>
-        <ul>
-          <li>Menu 1</li>
-          <li>Menu 2</li>
-          <li>Menu 3</li>
-        </ul>
+      <Home></Home>
 
-        <p>Contagem: {counter}</p>
-        <button onClick={incrementar}>Adicionar</button>
-      </Menu>
-
-      
       
     </Fragment>
   );
---
+++ frontend/src/pages/Home/index.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Home:React.FC = (props) => {
+    return(
+        <h1>Hello wolrd!!</h1>
+    );
+}
+
+export default Home;
